1ea62c8e74ca9a0ce97a97ad71dd0da4ab9ace77
PhongPT <phongzzz98@gmail.com>
2024-03-28 02:11:26 +0700

    test push

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -176,7 +176,7 @@ export default function Home() {
                           <Avatar src='https://api.dicebear.com/7.x/miniavs/svg?seed=8' />
                         }
                         title='LOL'
-                        description='Game Rác'
+                        description='Game rác'
                       />
                     </Card>
                   </Col>
